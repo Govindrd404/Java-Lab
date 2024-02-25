eab1d33ddfbc66821457ddbff035822b9e333a54
Mr Govind Rathod <156839830+MrGovindRD@users.noreply.github.com>
2024-02-26 00:56:47 +0530

    Update Quadratic.java

--- Quadratic.java
+++ Quadratic.java
@@ -13,35 +13,25 @@ class Quad {
     void Eval() {
         double det = b * b - 4 * a * c;
 
-    if (det > 0) {
+    if (det > 0)
+ {
 
-                        firstroot = (-b + Math.sqrt(det)) / (2 * a);
-                        secondroot = (-b - Math.sqrt(det)) / (2 * a);
+   firstroot = (-b + Math.sqrt(det)) / (2 * a);
+   secondroot = (-b - Math.sqrt(det)) / (2 * a);
+System.out.format("First Root = %.2f and Second Root = %.2f",
+firstroot, secondroot);
+   } else if (det == 0) {
 
-                        System.out.format(
-                                "First Root = %.2f and Second Root = %.2f",
-                                firstroot, secondroot);
-                }
+ firstroot = secondroot = -b / (2 * a);
+System.out.format("First Root = Second Root = %.2f;",firstroot);
+   } else {
 
-                else if (det == 0) {
+     double real = -b / (2 * a);
 
-                        firstroot = secondroot = -b / (2 * a);
+    double img = Math.sqrt(-det) / (2 * a);
 
-                        System.out.format(
-                                "First Root = Second Root = %.2f;",
-                                firstroot);
-                }
-
-                else {
-
-                        double real = -b / (2 * a);
-
-                        double img = Math.sqrt(-det) / (2 * a);
-
-                        System.out.printf("First Root = %.2f+%.2fi",
-                                                        real, img);
-                        System.out.printf("\nSecond Root = %.2f-%.2fi",
-                                                        real, img);
+  System.out.printf("First Root = %.2f+%.2fi", real, img);
+ System.out.printf("\nSecond Root = %.2f-%.2fi",real, img);
                 }
     }
 
